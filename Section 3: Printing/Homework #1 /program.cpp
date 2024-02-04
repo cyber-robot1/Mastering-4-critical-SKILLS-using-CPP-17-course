c3970e560e6bbfbbf86ac329fe1514bc10ff3aef
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-02-04 21:07:28 +0200

    Create program.cpp

---
+++ Section 3: Printing/Homework #1 /program.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// solution:
+
+/*
+
+
+Guess the output
+---
+25
+14/2
+7
+endl
+Practice makes perfect
+*/
+
+
+int main()
+{
+    cout << "\n\nGuess the "
+        << "output\n---\n";
+
+    cout << 10 + 20 - 5 << endl;
+    cout << "14/2" << "\n";
+    cout << 17 - 10 << "\n";
+    cout << "endl";
+    cout << "\nPractice makes perfect";
+
+    //cout << "THe way to get started is to quit talking and begin doing";
+    
+    return 0;
+
+    cout << "\n\nBye\n\n";
+}
