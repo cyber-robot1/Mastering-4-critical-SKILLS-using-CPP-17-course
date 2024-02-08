7241d6bd5cdcac410771e707b012cfd1bcd9411c
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-02-08 19:07:00 +0200

    Create program.cpp

---
+++ Section 4: Data Types and Variables/Homework #2 - Medium Challenges/Problem #2/program.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 8/2/2024
+*/
+
+#include <iostream>   
+using namespace std;
+
+int main()
+{
+    int num1, num2, num3 = -1;
+    cin >> num1 >> num2;
+    num3 = num1;
+    num1 = num2;
+    num2 = num3;
+    cout << num1 << " " << num2 << endl;
+
+    return 0;
+}
