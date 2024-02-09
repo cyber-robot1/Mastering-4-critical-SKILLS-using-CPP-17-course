4afe6c248c09bcf19460a6de49aaa5d75eb6edb5
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-02-09 18:35:47 +0200

    Create program.cpp

---
+++ Section 4: Data Types and Variables/Homework #3 - Hard Challenges/Problem #3/program.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 9/2/2024
+*/
+
+#include <iostream>   
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    cout << (N * (N + 1)) / 2;
+
+    return 0;
+}
