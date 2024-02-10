b14ed7a791a605ebbd410921a1238691e21dc048
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-02-11 00:03:51 +0200

    Create program.cpp

---
+++ Section 5: Operators/Division and Modulus Homework/Homework #2 - Medium Challenges/Problem #3/program.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 11/2/2024
+*/
+
+#include <iostream>   
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    n /= 1000;
+    cout << n % 10 << "\n";
+
+    return 0;
+}
