0741c6bb2a59c59bf5c82694e5b6938da6c11f7c
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-02-11 19:18:50 +0200

    Update program.cpp

--- Section 6: Selection/Homework #1 - Easy Challenges/Problem #4/program.cpp
+++ Section 6: Selection/Homework #1 - Easy Challenges/Problem #4/program.cpp
@@ -10,31 +10,15 @@ int main()
 {
     int X, x1, x2, x3, x4, x5;
     cin >> X >> x1 >> x2 >> x3 >> x4 >> x5;
-    int val1 = 0, val2 = 0;
+    int val = 0;
 
-    if (x1 <= X)
-        val1++;
-    if (x2 <= X)
-        val1++;
-    if (x3 <= X)
-        val1++;
-    if (x4 <= X)
-        val1++;
-    if (x5 <= X)
-        val1++;
-
-    if (x1 > X)
-        val2++;
-    if (x2 > X)
-        val2++;
-    if (x3 > X)
-        val2++;
-    if (x4 > X)
-        val2++;
-    if (x5 > X)
-        val2++;
+    val += (x1 <= X);
+    val += (x2 <= X);
+    val += (x3 <= X);
+    val += (x4 <= X);
+    val += (x5 <= X);
     
-    cout << val1 << " " << val2 << endl;
+    cout << val << " " << 5 - val << endl;
 
     return 0;
 }
