8474bd9b19968ebdfc22c6df0406d09498bccc4e
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-08 18:27:23 +0200

    Create program.cpp

---
+++ Section 7: Loops/While Loop Practice/Problem #1/program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    int X;
+    cin >> X;
+
+    int i = 1;
+    while (i <= X)
+    {
+        if (i % 3 == 0)
+            cout << i << " ";
+        i++;
+    }
+    
+    return 0;
+}
