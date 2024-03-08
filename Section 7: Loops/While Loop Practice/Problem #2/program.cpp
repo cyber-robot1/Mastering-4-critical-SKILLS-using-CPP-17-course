8fa6db36eff7da016e69cc95086003bf939b30b5
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-08 18:40:52 +0200

    Update program.cpp

--- Section 7: Loops/While Loop Practice/Problem #2/program.cpp
+++ Section 7: Loops/While Loop Practice/Problem #2/program.cpp
@@ -5,19 +5,22 @@ int main()
 {
     int X, Y;   
     cin >> X >> Y;
+    
     double pow = 1; 
-    if (Y < 0) 
+    
+    if (Y < 0)  // if exponent is negative.
     {
         Y *= -1;
         while (Y--)
             pow *= (1 / double(X));
     }
-    else
+    else   // if exponent is positive or zero.
     {
         while (Y--)      
             pow *= X;     
     }
-    cout << pow << endl;   
+    
+    cout << pow << endl;
     
     return 0;
 }
