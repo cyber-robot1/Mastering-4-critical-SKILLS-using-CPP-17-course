49bd789e011aac640b39d500f38bbddc5a5906e0
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-13 16:30:24 +0200

    Create program.cpp

---
+++ Section 7: Loops/While Loops Homework/Easy Challenges/Problem #2/program.cpp
@@ -0,0 +1,18 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 13/3/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    char c;
+    cin >> n >> c;
+    while (n--)
+        cout << c;
+    
+    return 0;
+}
