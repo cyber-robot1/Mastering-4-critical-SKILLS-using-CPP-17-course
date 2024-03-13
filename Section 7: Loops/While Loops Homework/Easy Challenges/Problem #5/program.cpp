94e3f52c45ac7747bd478e7e8d43a517011595d4
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-13 16:52:31 +0200

    Create program.cpp

---
+++ Section 7: Loops/While Loops Homework/Easy Challenges/Problem #5/program.cpp
@@ -0,0 +1,35 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 13/3/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int noODD = 0, sumODD = 0, noEVEN = 0, sumEVEN = 0;
+    int start = 1;
+    while (start <= n)
+    {
+        int num;
+        cin >> num;
+        if (start % 2 == 0)
+        {
+            sumEVEN += num;
+            noEVEN++;
+        }
+        else
+        {
+            sumODD += num;
+            noODD++;
+        }
+        start++;
+    }
+    cout << sumODD / noODD << " " << sumEVEN / noEVEN;
+    
+    return 0;
+}
