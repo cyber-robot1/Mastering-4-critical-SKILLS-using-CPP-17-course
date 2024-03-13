074dfa9b1537519149927c636aee74477d2c0709
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-13 16:27:50 +0200

    Create program.cpp

---
+++ Section 7: Loops/While Loops Homework/Easy Challenges/Problem #1/program.cpp
@@ -0,0 +1,20 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 13/3/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    while (x <= y)
+    {
+        cout << x << endl;
+        x++;
+    }
+    
+    return 0;
+}
