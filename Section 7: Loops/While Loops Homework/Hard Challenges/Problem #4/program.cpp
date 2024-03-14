9a8b11a3c2474d877c810cff63c59b7c797e61d0
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-14 18:58:36 +0200

    Create program.cpp

---
+++ Section 7: Loops/While Loops Homework/Hard Challenges/Problem #4/program.cpp
@@ -0,0 +1,36 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 14/3/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int sum = 0, pos = 1;
+        while (n--)
+        {
+            int num, cross = 1, tmp = pos;
+            cin >> num;
+            if (pos == 1)
+                cross = num;
+            else
+            {
+                while (tmp--)
+                    cross *= num;
+            }
+            sum += cross;
+            pos++;
+        }
+        cout << sum << endl;
+    }
+    
+    return 0;
+}
