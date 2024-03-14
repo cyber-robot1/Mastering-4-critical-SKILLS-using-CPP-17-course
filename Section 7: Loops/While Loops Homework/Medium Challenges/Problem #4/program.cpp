2170828c677fecc9422da108046805e464e1680a
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-14 02:55:59 +0200

    Create program.cpp

---
+++ Section 7: Loops/While Loops Homework/Medium Challenges/Problem #4/program.cpp
@@ -0,0 +1,38 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 14/3/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int mn = 0;
+        bool flag = true;
+        while (n--)
+        {
+            int num;
+            cin >> num;
+            if (flag)
+            {
+                mn = num;
+                flag = false;
+            }
+            else
+            {
+                if (num <= mn)
+                    mn = num;
+            }
+        }
+        cout << mn << endl;
+    }
+    
+    return 0;
+}
