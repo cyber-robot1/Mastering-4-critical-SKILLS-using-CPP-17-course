3a10b33958f6c5bb64a05d4a7f00b1272c40b246
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-15 02:47:18 +0200

    Update program.cpp

--- Section 7: Loops/For Loop Homework/Problem #4/program.cpp
+++ Section 7: Loops/For Loop Homework/Problem #4/program.cpp
@@ -18,7 +18,10 @@ int main()
         for (int i = 2; i <= n / 2; i++)
         {
             if (n % i == 0)
+            {
                 flag++;
+                break;
+            }
         }
 
         if (flag)
