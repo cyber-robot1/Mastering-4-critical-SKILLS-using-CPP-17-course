48bc2dcfddb4616a6baed689c8f54cda83a880cc
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-15 02:48:06 +0200

    Update program2.cpp

--- Section 7: Loops/For Loop Homework/Problem #4/program2.cpp
+++ Section 7: Loops/For Loop Homework/Problem #4/program2.cpp
@@ -18,7 +18,10 @@ int main()
         for (int i = 2; i <= sqrt(n); i++)
         {
             if (n % i == 0)
+            {
                 flag++;
+                break;
+            }
         }
 
         if (flag)
