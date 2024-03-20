8b1e90370bdfbd8707a9d1408d9e0ae43952cb9d
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-21 01:56:06 +0200

    Update program2.cpp

--- Section 8: 1D Arrays/1D Arrays Practice 1/Problem #2/program2.cpp
+++ Section 8: 1D Arrays/1D Arrays Practice 1/Problem #2/program2.cpp
@@ -13,16 +13,16 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    
-    // First max:
+
+    // First Max:
     int pos1 = 0;
     for (int i = 1; i < n; i++)
     {
         if (arr[i] >= arr[pos1])
             pos1 = i;
     }
-
-    // Second max:
+    /***********************************************/
+    // Second Max:
     int pos2 = 0;
     for (int i = 0; i < n; i++)
     {
@@ -37,7 +37,8 @@ int main()
         if (arr[i] >= arr[pos2] && i != pos1)
             pos2 = i;
     }
-
+    /************************************************/
+    // Maximum Pair Sum:
     cout << arr[pos1] + arr[pos2];
     
     return 0;
