4ab5c29949de923b015cada045d35e728b24b950
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-22 01:26:39 +0200

    Create program3.cpp

---
+++ Section 8: 1D Arrays/1D Arrays Practice 2/Problem #2/program3.cpp
@@ -0,0 +1,37 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 22/3/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int arr[200];
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int mxRepeatE = arr[0];
+    int mxValue = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int mxRepeat = 1;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+                mxRepeat++;
+        }
+        if (mxRepeat >= mxValue)
+        {
+            mxValue = mxRepeat;
+            mxRepeatE = arr[i];
+        }
+    }
+
+    cout << mxRepeatE;
+    
+    return 0;
+}
