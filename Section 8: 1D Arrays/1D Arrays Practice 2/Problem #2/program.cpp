c0b616f61e07ea92fcf5a7e2d7aaff29f9687416
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-21 19:49:09 +0200

    Create program.cpp

---
+++ Section 8: 1D Arrays/1D Arrays Practice 2/Problem #2/program.cpp
@@ -0,0 +1,48 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 21/3/2024
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[200];
+int freq[200];
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        freq[arr[i]]++;
+    }
+
+    // minimum element in array.
+    int mn = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] <= mn)
+            mn = arr[i];
+    }
+
+    // maximum element in array.
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] >= mx)
+            mx = arr[i];
+    }
+
+    // most frequent element 
+    int mxFreq = freq[mx];
+    for (int i = mn; i <= mx; i++)
+    {
+        if (freq[i] >= mxFreq)
+            mxFreq = freq[i];
+    }
+
+    cout << mxFreq;
+    
+    return 0;
+}
