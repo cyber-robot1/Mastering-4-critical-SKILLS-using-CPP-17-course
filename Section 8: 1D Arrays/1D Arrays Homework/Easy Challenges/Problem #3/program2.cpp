0e70dd11d25bc73094c5c3d60bd93a4b6acc71a6
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-22 18:04:00 +0200

    Create program2.cpp

---
+++ Section 8: 1D Arrays/1D Arrays Homework/Easy Challenges/Problem #3/program2.cpp
@@ -0,0 +1,32 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 22/3/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int k = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        cin >> x;
+      
+        if (i > 1)
+        {
+            if (x != k)
+                cout << x << " ";
+        }
+        else
+            cout << x << " ";
+      
+        k = x;
+    }
+    
+    return 0;
+}
