0cca6269b0fc73b71d4d35b989160270fc227f47
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-03-22 21:08:11 +0200

    Create program.cpp

---
+++ Section 8: 1D Arrays/1D Arrays Homework/Medium Challenges/Problem #4/program.cpp
@@ -0,0 +1,29 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 22/3/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int arr[201], freq[10];
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        int num = arr[i];
+        while (num)
+        {
+            freq[num % 10]++;
+            num /= 10;
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+        cout << i << " " << freq[i] << endl;
+    
+    return 0;
+}
