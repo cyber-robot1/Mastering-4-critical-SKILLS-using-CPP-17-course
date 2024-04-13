12c4de3868f097de54ecd679a87a21d7758df03a
Karim Tarek Ibrahim <76911827+cyber-robot1@users.noreply.github.com>
2024-04-13 05:56:16 +0200

    Create program2.cpp

---
+++ Section 10: Multidimensional Arrays/Multidimensional Arrays Homework/Easy Challenges/Problem #3/program2.cpp
@@ -0,0 +1,33 @@
+/*
+Author: Karim Tarek Ibrahim
+Date: 1/4/2024
+*/
+#include <iostream>
+using namespace std;
+
+int arr[100][100], transpose[100][100];
+int main()
+{
+	int row, col;
+	cin >> row >> col;
+
+	// Input the array then transposing it:
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			cin >> arr[i][j];
+			transpose[j][i] = arr[i][j];
+		}
+	}
+
+	// Printing the transposed matrix:
+	for (int i = 0; i < col; i++)
+	{
+		for (int j = 0; j < row; j++)
+			cout << transpose[i][j] << " ";
+		cout << endl;
+	}
+	
+	return 0;
+}
